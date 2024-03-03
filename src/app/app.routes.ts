4c7070d0adb9fd3d4d57ba95fd59dac204a981b1
Shrikant Jagtap <shrikantjagtap0194@gmail.com>
2024-03-03 23:48:24 +0530

    Portfolio using AngularJS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {'path':'home', component:HomeComponent},
+  {'path':'about', component:AboutComponent},
+  {'path':'skill', component:SkillsComponent},
+  {'path':'project', component:ProjectComponent},
+  {'path':'contact', component:ContactComponent},
+
+];
